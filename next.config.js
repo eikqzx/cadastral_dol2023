cea373e6eafe3888f05d5737eb9312cfc0cace41
Attapon Sroysuwan <55727960+eikqzx@users.noreply.github.com>
2023-04-19 15:12:52 +0700

    Q

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  api: {
+    responseLimit: false,
+  },
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig, env: {
+    hostAPI: 'http://127.0.0.1:8011',
+    hostUploadAPI: 'http://127.0.0.1:8099',
+    privateKey :"imyourfather!!!",
+    NEXTAUTH_SECRET: "imyourfather!!!",
+    hostPdf: 'http://localhost/'
+  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production",
+  // },
+}
