14345d965c477e71da8ba0bd04a3d2b77e93f36c
Attapon Sroysuwan <55727960+eikqzx@users.noreply.github.com>
2023-04-25 08:12:42 +0700

    Revert "Q"
    This reverts commit d32709b17fd6b0eaf1cd9aaf45d790d3a44ef63e.

--- pages/asignJobs/index.js
+++ pages/asignJobs/index.js
@@ -48,12 +48,13 @@ export default function IndexAccountControl(props) {
     const [open, setOpen] = React.useState(false);
     const [message, setMessage] = React.useState('');
     const [type, setType] = React.useState('');
-    const [processSeq, setProcessSeq] = React.useState(102)
+    const [printplateTypeData, setPrintplateTypeData] = React.useState([]);
+    const [processSeq, setProcessSeq] = React.useState(101)
     const [userData, setUserData] = React.useState(null);
     const [landOffice, setLandOffice] = React.useState(null);
     const [pdfData, setPdfData] = React.useState();
-    const [searchParameter, setSearchParameter] = React.useState(null);
     const { data } = useSession();
+
 
     // console.log(landOffice, "landOffice");
     // console.log(userData, "userData");
@@ -72,16 +73,10 @@ export default function IndexAccountControl(props) {
     React.useEffect(() => {
         if (isNotEmpty(dataUrl)) {
             let seq = decode(dataUrl?.PROCESS_SEQ);
-            console.log(seq, "seq dataUrl");
+            console.log(seq, "seq");
             setProcessSeq(seq);
         }
     }, [])
-
-    React.useEffect(() => {
-        if (searchData.length == 0) {
-            setTapData([]);
-        }
-    }, [searchData])
 
     React.useEffect(() => {
         setUserData(data?.user);
@@ -93,16 +88,32 @@ export default function IndexAccountControl(props) {
     // console.log(userData,"userData");
     // console.log(processSeq, "processSeq");
 
+    const reqPrintplateType = async () => {
+        let res = await getPrintPlateType();
+        console.log(res, "reqPrintplateType");
+        setPrintplateTypeData(filterRecordStatus(res.rows));
+    }
+
+    const handleChange = (event, newValue) => {
+        setValueTab(newValue);
+        setDocType(newValue);
+        setTapData([]);
+        setSearchData([]);
+    };
+
     const onSearchNew = async (obj) => {
         console.log(obj, "obj_onSearch");
-        setSearchParameter(obj);
         setPdfData(obj)
         let data = null;
         data = await cadastralImage102ByConditionParcelNoTo(obj);
         data = data.rows
-        console.log(data, "onSearchNew");
+        console.log(data,"onSearchNew");
         setSearchData(data)
     }
+
+    React.useEffect(() => {
+        reqPrintplateType();
+    }, [])
 
     return (
         <Grid container spacing={0.5} py={7.5}>
@@ -117,7 +128,7 @@ export default function IndexAccountControl(props) {
                                 background: 'linear-gradient(26deg, rgba(255,255,232,1) 20%, rgba(188,243,176,1) 100%) !important',
                             }}
                         >
-                            <Typography >ค้นหารับงานสแกน</Typography>
+                            <Typography >ค้นหารายการบัญชีคุม เบิก-จ่าย</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Search provinceSeq={landOffice?.PROVINCE_SEQ} landOfficeSeq={landOffice?.LANDOFFICE_SEQ} disabled={['licensePage', 'fileNo', 'parcelNO', 'landNo']} onSearch={onSearchNew} onReset={setSearchData} />
@@ -125,9 +136,12 @@ export default function IndexAccountControl(props) {
                     </Accordion>
                 </Grid>
             }
-            <Grid item xs={10} md={12}>
+            <Grid item xs={2} md={2}>
+                <SideTreeView data={searchData} setTapData={setTapData} process={processSeq} />
+            </Grid>
+            <Grid item xs={10} md={10}>
                 <Paper sx={{ height: "100vh", flexGrow: 1, overflowY: 'auto' }}>
-                    <Tab1 searchData={searchData} process={processSeq} onSearch={onSearchNew} pdfData={pdfData} searchParameter={searchParameter} />
+                    <Tab1 tabData={tabData} searchData={searchData} onSearch={onSearchNew} pdfData={pdfData}/>
                 </Paper>
             </Grid>
         </Grid>
--- pages/components/sideTreeView/index.js
+++ pages/components/sideTreeView/index.js
@@ -142,7 +142,7 @@ export default function SideTreeView(props) {
                     item.DATA = statusSeq104
                 }
             })
-            console.log(masStatus, "onSearchData masStatus");
+            console.log(masStatus, "masStatus");
             await setTreeViewData(masStatus.sort((a, b) => b.STATUS_SEQ - a.STATUS_SEQ));
         }
     }
