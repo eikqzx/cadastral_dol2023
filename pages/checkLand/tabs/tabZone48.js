9777a8e700f42c038442c0c59ab888096561e377
Jack.AK <91724656+OutLooKAVS@users.noreply.github.com>
2023-07-07 15:38:00 +0700

    jack

--- pages/checkLand/tabs/tabZone48.js
+++ pages/checkLand/tabs/tabZone48.js
@@ -8,11 +8,12 @@ import CheckLandMap from "@/pages/checkLand/components/checkLandMap";
 import CheckLandList from "@/pages/checkLand/components/checkLandList";
 import CheckLandImage from "@/pages/checkLand/components/checkLandImage";
 export default function TabZone48Index(props) {
+    const [layerSeq, setLayerSeq] = React.useState(102)
     return (
         <Grid container columns={24}>
             <Grid item xs={12}>
                 <Grid sx={{ height: "50vh" }} component={Paper} elevation={2}>
-                    <CheckLandMap />
+                    <CheckLandMap layerSeq={layerSeq} />
                 </Grid>
                 <Grid sx={{ height: "30vh" }} component={Paper} elevation={2}>
                     <CheckLandList />
---
+++ service/mas/layer/index.js
@@ -0,0 +1,56 @@
+import { AddLoading, RemoveLoading } from "@/pages/components/loadingscreen";
+import axios from "axios";
+
+export async function getLayer() {
+    let url = `/api/mas/layer/getLayer`
+    AddLoading();
+    try {
+        let res = await axios.get(url)
+        let data = res.data
+        //console.log(data)
+        RemoveLoading();
+        return data
+    } catch (err) {
+        console.log(err)
+        RemoveLoading();
+        return false
+    }
+}
+
+export async function getLayerByPK(seq) {
+    let url = `/api/mas/layer/getLayerByPK`
+    let dataSend = {
+        seq: seq
+    }
+    AddLoading();
+    try {
+        let res = await axios.post(url, dataSend)
+        let data = res.data
+        //console.log(data)
+        RemoveLoading();
+        return data
+    } catch (err) {
+        console.log(err)
+        RemoveLoading();
+        return false
+    }
+}
+
+export async function getLayerByType(seq) {
+    let url = `/api/mas/layer/getLayerByType`
+    let dataSend = {
+        seq: seq
+    }
+    AddLoading();
+    try {
+        let res = await axios.post(url, dataSend)
+        let data = res.data
+        //console.log(data)
+        RemoveLoading();
+        return data
+    } catch (err) {
+        console.log(err)
+        RemoveLoading();
+        return false
+    }
+}
